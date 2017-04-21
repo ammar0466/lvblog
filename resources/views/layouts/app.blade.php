50e1e998280e682ece09cbc1124f2aa2311cf965
ammar0466 <ammar0466@gmail.com>
2017-04-21 11:08:10 +0800

    siap edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    My BLog
                 </a>
             </div>
 
